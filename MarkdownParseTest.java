e1a95fce53e9e7728a15589c4ce4952eab302af3
imsanika03 <imsanika03@gmail.com>
2022-04-22 09:04:21 -0700

    added four working tests on files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -15,13 +15,65 @@ public class MarkdownParseTest {
     }
 
     @Test 
-    public void checkParse() throws IOException{
+    public void checkParseTest1() throws IOException{
         ArrayList<String> expected = new ArrayList<>(); 
         expected.add("https://something.com"); 
         expected.add("some-page.html"); 
        
 
         Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content); 
+        
+        assertEquals(expected, links);
+    }
+
+    @Test 
+    public void checkParseTest2() throws IOException{
+        ArrayList<String> expected = new ArrayList<>(); 
+        expected.add("https://something.com"); 
+        expected.add("some-thing.html"); 
+       
+
+        Path fileName = Path.of("test2-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content); 
+        
+        assertEquals(expected, links);
+    }
+
+    @Test 
+    public void checkParseTest3() throws IOException{
+        ArrayList<String> expected = new ArrayList<>(); 
+        expected.add("https://something.com"); 
+        expected.add("https://something.com"); 
+        expected.add("some-thing.html"); 
+        expected.add("https://something.com"); 
+        expected.add("some-thing.html"); 
+        expected.add("https://something.com"); 
+        expected.add("some-thing.img"); 
+       
+
+        Path fileName = Path.of("test3-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content); 
+        
+        assertEquals(expected, links);
+    }
+
+    @Test 
+    public void checkParseTest4() throws IOException{
+        ArrayList<String> expected = new ArrayList<>(); 
+        expected.add("https://something.com"); 
+        expected.add("https://something.com"); 
+        expected.add("some-thing.html"); 
+        expected.add("https://something.com"); 
+        expected.add("some-thing.html"); 
+        expected.add("https://something.com"); 
+        expected.add("some-thing.html"); 
+       
+
+        Path fileName = Path.of("test4-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content); 
         
