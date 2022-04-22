43b6440f3981de1e601303696bf50c2233d82d47
imsanika03 <imsanika03@gmail.com>
2022-04-22 08:46:20 -0700

    javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest

---
+++ MarkdownParseTest.java
@@ -0,0 +1,13 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.*;
+public class MarkdownParseTest {
+
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+
+}
